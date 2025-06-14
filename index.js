15090be6e26ac1197eb107b7c9b3759a25b0ae79
Simon Ndege <simopetley@gmail.com>
2025-06-14 22:16:59 +0300

    initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+console.log(customerName); // 👉 "BOB"
+
+
+
+function setBestCustomer() {
+    bestCustomer = "not Bob"; // ✅ no 'var' means it's global (not best practice, but good for learning)
+}
+
+setBestCustomer();
+console.log(bestCustomer); // 👉 "not Bob"
+
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
+}
+overwriteBestCustomer()
+console.log(bestCustomer)
+
+const leastFavoriteCustomer="Bob";
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="June"
+ }
+
+ changeLeastFavoriteCustomer();
+ console.log(leastFavoriteCustomer);
